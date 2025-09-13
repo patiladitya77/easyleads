a1605f64ce6d05c48c5393e7564e76397821c88e
patiladitya77 <adpatil587@gmail.com>
2025-09-13 18:43:30 +0530

    API: getallleads

--- src/app/api/buyers/route.ts
+++ src/app/api/buyers/route.ts
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { baseBuyerSchema } from "@/lib/validation";
 import { NextResponse } from "next/server";
 
+//API for creater buyer lead
 export async function POST(req: Request) {
   try {
     const body = await req.json();
@@ -14,6 +15,21 @@ export async function POST(req: Request) {
     });
 
     return NextResponse.json(buyer, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+    return NextResponse.json({ error: String(error) }, { status: 400 });
+  }
+}
+
+//API for getting all the leads
+export async function GET() {
+  try {
+    const buyers = await prisma.buyer.findMany({
+      orderBy: { updatedAt: "desc" },
+    });
+    return NextResponse.json(buyers);
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json({ error: error.message }, { status: 400 });
