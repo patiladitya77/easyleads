21f20634f6d8e3155cb3be3421597317e5712406
patiladitya77 <adpatil587@gmail.com>
2025-09-14 12:41:50 +0530

    UI: Dashboard

--- src/app/home/buyers/page.tsx
+++ src/app/home/buyers/page.tsx
@@ -2,14 +2,33 @@ import CreateLead from "@/components/CreateLead";
 
 export default function Dashboard() {
   return (
-    <div>
-      <div className="flex justify-between">
-        <p>Easy Leads Dashboard</p>
-        <button className="bg-black text-white rounded-md px-3 py-2">
-          View all Leads
+    <div className="h-screen bg-gray-50 p-6">
+      <div className="flex items-center justify-between mb-6">
+        <h1 className="text-2xl font-bold text-gray-800">
+          Easy Leads Dashboard
+        </h1>
+        <button className="bg-black hover:bg-gray-800 text-white rounded-lg px-4 py-2 transition cursor-pointer">
+          <div className="flex">
+            <p>View All Leads</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 mx-1"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </div>
         </button>
       </div>
-      <div>
+
+      <div className="bg-white shadow-md rounded-xl p-6">
         <CreateLead />
       </div>
     </div>
--- src/components/CreateLead.tsx
+++ src/components/CreateLead.tsx
@@ -4,14 +4,36 @@ import React from "react";
 
 const CreateLead = () => {
   const router = useRouter();
+
   const handleClick = () => {
     router.push("/home/buyers/new");
   };
 
   return (
-    <div className="flex justify-between mx-10 bg-gray-100 shadow-md border border-gray-200">
-      <p>Got a new Lead?</p>
-      <button onClick={handleClick}>Create Here</button>
+    <div className="flex items-center justify-between mx-10 p-4 bg-white shadow-sm border border-gray-200 rounded-lg">
+      <p className="text-gray-700 font-medium">Got a new lead?</p>
+      <button
+        onClick={handleClick}
+        className="bg-black hover:bg-gray-800 text-white px-4 py-2 rounded-lg transition cursor-pointer"
+      >
+        <div className="flex ">
+          <p>Create Here</p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 mx-1"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+        </div>
+      </button>
     </div>
   );
 };
